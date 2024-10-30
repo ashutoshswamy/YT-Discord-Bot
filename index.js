429da660a4b098ac11f80547fbd6d232f26f9620
Ashutosh Swamy <76511506+ashutoshswamy@users.noreply.github.com>
2024-10-30 19:55:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const config = require("./config.json");
+const discord = require("discord.js");
+
+const client = new discord.Client({
+  intents: [
+    discord.GatewayIntentBits.Guilds,
+    discord.GatewayIntentBits.GuildMessages,
+    discord.GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(config.bot_token);
